57ddb92faccebfb91e52abaebe6f7e0334cf2243
SANVITHA1605 <85791390+SANVITHA1605@users.noreply.github.com>
2023-02-22 15:11:39 +0530

    Add files via upload

---
+++ prog1.rb
@@ -0,0 +1,8 @@
+puts "hello world"
+puts "HEIGHT"
+ht=gets.chomp.to_i
+puts "this takes integer input"
+gt=gets.chomp.to_f
+puts "this takes float input"
+puts "your ht is #{ht}"
+puts "your marks are #{gt}"
